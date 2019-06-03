306442a417eaa8c869aaef9b0b2af7a6bb122ec4
Kevin Huang <kevin8656@hotmail.com>
2019-06-03 16:36:26 +0800

    Add PokerGameTest.cs and PokerGame.cs

---
+++ PokerGame/PokerGame.cs
@@ -0,0 +1,10 @@
+﻿namespace PokerGame
+{
+    public class PokerGame
+    {
+        public PokerGame(string first, string second)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ PokerGame/PokerGameTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace PokerGame
+{
+    public class PokerGameTest
+    {
+        [Test]
+        public void full_house_vs_four_of_a_kind()
+        {
+            var pokerGame = new PokerGame("S9,DA,CA,C9,H9","S2,D2,H2,C2,D9");
+        }
+    }
+}
